d10fa6cfd6b9819584bb6607e66dfa44b8a6e62e
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2021-08-31 20:15:55 +0200

    test image upgrade build

--- build.sh
+++ build.sh
@@ -8,12 +8,13 @@ fi
 
 # define variables
 DOWNLOAD_URL="https://releases.ubuntu.com/20.04.2.0/ubuntu-20.04.2.0-desktop-amd64.iso"
+DOWNLOAD_URL="https://releases.ubuntu.com/20.04/ubuntu-20.04.3-desktop-amd64.iso"
 BUILD_DIR="build"
 ISO_EXTRACTED_DIR="${BUILD_DIR}/extracted-iso"
 ISO_MOUNT_DIR="${BUILD_DIR}/extracted-iso"
 SQUASHFS_EXTRACTED_DIR="${BUILD_DIR}/squashfs"
 TODAY="$(date +'%Y%m%d')"
-ISO_FILENAME="${BUILD_DIR}/ubuntu-20.04.2.0.iso"
+ISO_FILENAME="${BUILD_DIR}/ubuntu-20.04.3.iso"
 ISO_VOLUME_ID="heyalter-${TODAY}-${CI_PIPELINE_IID}"
 IMAGE_NAME="heyalter-${CI_COMMIT_REF_NAME}-${TODAY}-b${CI_PIPELINE_IID}-${CI_COMMIT_SHORT_SHA}.iso"
 IMAGE_META_NAME="${IMAGE_NAME/\.iso/.release.txt}"
@@ -70,7 +71,7 @@ env | grep -e CI_PIPELINE_IID \
   ## extract iso
   log "Extract iso to $ISO_EXTRACTED_DIR"
   7z x -o$ISO_EXTRACTED_DIR ${ISO_FILENAME}
-  
+
   log "unsquash squashfs"
   fakeroot unsquashfs -d ${SQUASHFS_EXTRACTED_DIR} ${ISO_EXTRACTED_DIR}/casper/filesystem.squashfs
   #
@@ -84,7 +85,7 @@ env | grep -e CI_PIPELINE_IID \
   # ln -s /sys ${SQUASHFS_EXTRACTED_DIR}/
   # #ln -s /dev/pts ${SQUASHFS_EXTRACTED_DIR}/dev/
   #
-  
+
   log "installing software inside chroot"
   fakeroot fakechroot chroot ${SQUASHFS_EXTRACTED_DIR} add-apt-repository -y universe
   fakeroot fakechroot chroot ${SQUASHFS_EXTRACTED_DIR} add-apt-repository -y multiverse
@@ -93,7 +94,7 @@ env | grep -e CI_PIPELINE_IID \
   fakeroot fakechroot chroot ${SQUASHFS_EXTRACTED_DIR} apt install -y default-jre geogebra gimp vlc mumble enigmail keepass2 audacity geany obs-studio openscad krita krita-l10n # gawk mawk  #
   fakeroot fakechroot chroot ${SQUASHFS_EXTRACTED_DIR} apt install -y vim pwgen sl neovim curl youtube-dl gparted # gawk mawk
   fakeroot fakechroot chroot ${SQUASHFS_EXTRACTED_DIR} apt clean
-  
+
   log "Fixing broken symlinks"
   FULL_SQFS_EXTRACTED_DIR=`realpath $SQUASHFS_EXTRACTED_DIR`
   fixsymlinks() {
@@ -108,25 +109,25 @@ env | grep -e CI_PIPELINE_IID \
   }
   fixsymlinks /usr/bin
   fixsymlinks /etc/alternatives
-  
+
   log "Add our own Grub configuration"
   cp files/grubdefault ${SQUASHFS_EXTRACTED_DIR}/etc/default/grub
-  
+
   log "Copying our files to the ISO"
   cp files/preseed/* ${ISO_EXTRACTED_DIR}/preseed/
   cp files/isolinux.cfg ${ISO_EXTRACTED_DIR}/isolinux/isolinux.cfg
   rm -f ${ISO_EXTRACTED_DIR}/boot/grub/grub.cfg
   cp files/grub.cfg ${ISO_EXTRACTED_DIR}/boot/grub/grub.cfg
-  
-  
+
+
   log "Homedir"
   cp -R files/homeschule ${SQUASHFS_EXTRACTED_DIR}/home/schule
   chown -R 1000:1000 ${SQUASHFS_EXTRACTED_DIR}/home/schule
   chmod -R 755 ${SQUASHFS_EXTRACTED_DIR}/home/schule/.config/
-  
+
   log "Hey Alter release information"
   cp $ARTIFACTS_DIR/${IMAGE_META_NAME} ${SQUASHFS_EXTRACTED_DIR}/etc/heyalter-release
-  
+
   log "Setup scripts"
   cp -R setup ${ISO_EXTRACTED_DIR}/
   ./tools/bashwrapper/convert.sh ${ISO_EXTRACTED_DIR}/setup/setuproot.sh
@@ -134,10 +135,10 @@ env | grep -e CI_PIPELINE_IID \
   ./tools/bashwrapper/convert.sh ${ISO_EXTRACTED_DIR}/setup/54KernelDebs/changeTo54KernelOffline.sh
   git clone https://gitli.stratum0.org/heyalter/desktop-mainline-kernel.git ${ISO_EXTRACTED_DIR}/setup/desktop-mainline-kernel
   rm -fR ${ISO_EXTRACTED_DIR}/setup/desktop-mainline-kernel/.git
-  
+
   log "Persistent HA directory"
   cp -R heyalter ${ISO_EXTRACTED_DIR}/
-  
+
   #
   #
   #
@@ -146,17 +147,17 @@ env | grep -e CI_PIPELINE_IID \
   # rm -rf ${SQUASHFS_EXTRACTED_DIR}/tmp/* ${SQUASHFS_EXTRACTED_DIR}/root/.bash_history
   # rm -rf ${SQUASHFS_EXTRACTED_DIR}/{run,dev,proc,sys}
   # mkdir ${SQUASHFS_EXTRACTED_DIR}/{run,dev,proc,sys}
-  
+
   log "After Install Script"
   cp -R files/homeschule ${ISO_EXTRACTED_DIR}/
-  
+
   cp usb_after_install/* ${ISO_EXTRACTED_DIR}
   chown 1000:1000 ${ISO_EXTRACTED_DIR}/setup_lokal.sh
-  
-  
+
+
   log "Download Snaps"
   ./_download_snaps.sh ${ISO_EXTRACTED_DIR}/setup/snaps
-  
+
   log "Download HEY-HILFE-Support-Handbuch.pdf"
   curl https://heyalter.com/wp-content/uploads/2021/02/HEY-HILFE-Support-Handbuch.pdf --output ${ISO_EXTRACTED_DIR}/setup/HEY-HILFE-Support-Handbuch.pdf --fail --location --max-time 60
 
