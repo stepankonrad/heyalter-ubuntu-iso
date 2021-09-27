b39042a0cad0436b54efe9a32a4da6b5a29bced7
Andre <rokka@gmx.net>
2021-09-27 13:47:21 +0200

    Screensaver Lock und Lockscreen deaktiviert

--- setup/cleanup.sh
+++ setup/cleanup.sh
@@ -11,4 +11,10 @@ cp /opt/setup/heyalterhelp.desktop ~/Schreibtisch
 gio set ~/Schreibtisch/heyalterhelp.desktop "metadata::trusted" true
 # HEY-HILFE-Support-Handbuch.pdf is downloaded to /setup in build.sh
 cp /opt/setup/HEY-HILFE-Support-Handbuch.pdf ~/Schreibtisch
+
+# Screensaver und Lock-Screen reaktivieren
+gsettings set org.gnome.desktop.screensaver lock-enabled 'true'
+gsettings set org.gnome.desktop.lockdown disable-lock-screen 'false'
+gsettings set org.gnome.desktop.session idle-delay 300
+
 /opt/setup/cleanuproot.sh
--- setup/setup.sh
+++ setup/setup.sh
@@ -5,12 +5,16 @@
 
 zenity --info --text "$(lshw -C memory)\n------------------------------------\nAnzahl Kerne: $(nproc)\n------------------------------------\n$(lshw -C cpu)" --width 1024
 
+# Screensaver und Lock-Screen deaktivieren
+gsettings set org.gnome.desktop.screensaver lock-enabled 'false'
+gsettings set org.gnome.desktop.lockdown disable-lock-screen 'true'
+gsettings set org.gnome.desktop.session idle-delay 0
+
 # Rechte der kopierten Dateien fixen
 gnome-terminal --wait -- bash -c "/opt/setup/setuproot.sh"
 
 # einstellen der favoriten
 dconf write /org/gnome/shell/favorite-apps "['chromium_chromium.desktop', 'thunderbird.desktop', 'org.gnome.Nautilus.desktop', 'libreoffice-writer.desktop', 'libreoffice-calc.desktop', 'libreoffice-impress.desktop', 'org.gnome.Software.desktop']"
-
 
 # zeige nach reboot bei erster verbindung die wilkommen-seite
 systemctl enable --user heyalter.service
