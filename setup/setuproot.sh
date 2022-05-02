e638d4e74aa8492558b367e58bec224d91aed73f
Sven <383-5ven@users.noreply.gitli.stratum0.org>
2022-05-02 16:02:26 +0200

    Fixing alternatives in the setup script
    The chroot install seems to not correctly apply the
    update-alternatives. Therefore this is fixed in the
    setup script.
    
    Without this fix, OBS cannot be started.

--- setup/setuproot.sh
+++ setup/setuproot.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# Fix update-alternative symlinks which get broken when installing software in chroot
+for l in $(ls /etc/alternatives)
+do
+	update-alternatives --auto $l || true
+done
+
 cd /opt/setup/snaps
 ./_install_all_snaps.sh
 touch /opt/setup/setup_done
