dd1fe7e1721330d2a549233bb73da8f86deaf81d
Thomas Hottendorff <thomas.hottendorff@hvbo4.de>
2023-02-02 12:49:52 +0100

    Added snaps core22 and gnome-42-2204 required by discord.

--- _download_snaps.sh
+++ _download_snaps.sh
@@ -2,10 +2,10 @@
 
 OUTPUT_DIR=$1
 
-for snap2install in core18 core20 core gnome-3-28-1804 gnome-3-38-2004 cups chromium teams teams-for-linux skype discord
+for snap2install in core18 core20 core22 core gnome-3-28-1804 gnome-3-38-2004 gnome-42-2204 cups chromium teams teams-for-linux skype discord
 do
-	echo download $directory
-	snap download $snap2install --target-directory ${OUTPUT_DIR}/
-  mv ${OUTPUT_DIR}/${snap2install}_*.snap ${OUTPUT_DIR}/${snap2install}.snap
-  mv ${OUTPUT_DIR}/${snap2install}_*.assert ${OUTPUT_DIR}/${snap2install}.assert
+    echo download $directory
+    snap download $snap2install --target-directory ${OUTPUT_DIR}/
+    mv ${OUTPUT_DIR}/${snap2install}_*.snap ${OUTPUT_DIR}/${snap2install}.snap
+    mv ${OUTPUT_DIR}/${snap2install}_*.assert ${OUTPUT_DIR}/${snap2install}.assert
 done
--- setup/snaps/_install_all_snaps.sh
+++ setup/snaps/_install_all_snaps.sh
@@ -9,11 +9,17 @@ snap install core18.snap
 snap ack core20.assert
 snap install core20.snap
 
+snap ack core22.assert
+snap install core22.snap
+
 snap ack gnome-3-28-1804.assert
 snap install gnome-3-28-1804.snap
 
 snap ack gnome-3-38-2004.assert
 snap install gnome-3-38-2004.snap
+
+snap ack gnome-42-2204.assert
+snap install gnome-42-2204.snap
 
 snap ack cups.assert
 snap install cups.snap
