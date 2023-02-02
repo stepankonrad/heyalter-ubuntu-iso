2dd074d0ffc39e6c9ddb766be2c3d8c664c69b57
Thomas Hottendorff <thomas.hottendorff@hvbo4.de>
2023-02-02 11:18:13 +0100

    Refactored script to make it a little faster and to remove failing snap installs.

--- setup/snaps/_install_all_snaps.sh
+++ setup/snaps/_install_all_snaps.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-IFS=$'\n'
-SNAP_ASSERTS_ARR=($(find . -name "*.assert"))
-SNAPS_ARR=($(find . -name "*.snap"))
-unset IFS
-
-SNAP_ASSERTS=$(printf "%s " "${SNAP_ASSERTS_ARR[@]}")
-SNAPS=$(printf "%s " "${SNAPS_ARR[@]}")
-
 snap ack core.assert
 snap install core.snap
 
@@ -26,18 +18,27 @@ snap install gnome-3-38-2004.snap
 snap ack cups.assert
 snap install cups.snap
 
-for snap2install in $SNAP_ASSERTS
+# set arrays with all available snaps
+mapfile -d '' SNAP_ASSERTS_ARR < <(find . -name "*.assert" -print0)
+mapfile -d '' SNAPS_ARR < <(find . -name "*.snap" -print0)
+
+for snap2install in "${SNAP_ASSERTS_ARR[@]}"
 do
+    if ! snap list $( basename $snap2install .assert ) &> /dev/null
+    then   
 	echo snap ack $snap2install
 	snap ack $snap2install
-done
-
-for snap2install in $SNAPS
-do
-  if [ $snap2install != "./skype.snap" ]; then
-    echo snap install $snap2install
-    snap install $snap2install
-  fi
+    fi
 done
 
 snap install --classic ./skype.snap
+
+for snap2install in "${SNAPS_ARR[@]}"
+do
+    if ! snap list $( basename $snap2install .snap ) &> /dev/null
+    then   
+	echo snap install $snap2install
+	snap install $snap2install
+    fi
+done
+
