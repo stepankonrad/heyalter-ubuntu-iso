0444c22b999f058c3e7d26717f3932792c70e943
Peter Sattler <psattler@hasendamm.de>
2024-07-20 13:11:08 +0200

    Network Skript angepasst & automatisches Update nur im HeyAlter Setup WLAN (mit Proxy); alle anderen müssen es manuell anstossen, wenn gewünscht

--- setup/steps/060_network_script.sh
+++ setup/steps/060_network_script.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
-
+#
 # Testen auf eine WLAN-Karte
+#
 if ! nmcli d wifi rescan; then
    zenity --warning --icon=network-wireless --width=650 \
 	  --text "Keine funktionierende WLAN-Karte gefunden! Bitte noch eine einbauen, den Treiber installieren oder einen WLAN-Stick beilegen."
 fi
+#
+# Wenn schon Netzwerk vorhanden ist weiter nichts zu tun 
+# Diese Logik geht implizit davon aus, dass das HeyAlter Setup Netzwerk zu diesem Zeitpunkt NICHT schon aktiv ist 
+#
+if [ $(nmcli n connectivity check) == "full" ]; then
+   echo "Netzwerk ist oben"
+   exit 0
+fi
 
 #
-#  Wenn HeyAlter Setup WLAN vorhanden, dann die Daten sammeln & schicken
+#  Wenn HeyAlter Setup WLAN vorhanden, dann verbinden und die Daten sammeln & schicken
 #
 
 if nmcli dev wifi connect 'HeyAlter Setup'; then
 
    REPLY=$(zenity --title="Rechnernummer eingeben" --entry --text="Rechnernummer ohne BS- eingeben (bsp. 1234 für den Rechner BS-1234 eingeben)")
 
-   sudo /opt/setup/setup_steps/sammle_information $REPLY 2>&1 | tee -a $LOGFILE
+   sudo $BINDIR/steps/sammle_information $REPLY 2>&1 | tee -a $LOGFILE
+#
+# Aufruf Update Software nur lokal in BS 
+#
+   sudo $BINDIR/steps/_update_software.sh
+   return $?
 
-else
-   zenity --warning --icon=network-error --width=650 \
-      --text="Verbindung zu HeyAlter Setup nicht aufgebaut; bitte checken, ggf. eigene Verbindung?"
 fi
+#
+# Kein Netzwerk vorhanden!
+#
+zenity --warning --icon=network-error --width=650 \
+      --text="Es ist keine Netzwerkverbindung aktiv" 
 
