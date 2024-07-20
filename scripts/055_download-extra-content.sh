7fb5e4faa1cd100db17bf7a1841989c6660369a8
Peter Sattler <psattler@hasendamm.de>
2024-07-20 11:52:51 +0200

    Download snaps in download_extra_content eingebunden; Download Hilfe-PDF in Background, da es langsam ist

--- scripts/055_download-extra-content.sh
+++ scripts/055_download-extra-content.sh
@@ -4,8 +4,16 @@
 cd $(dirname "$0")
 . env.sh
 
-log "Download Snaps"
-./_download_snaps.sh "$ISO_EXTRACTED_DIR/setup/snaps"
-
 log "Download HEY-HILFE-Support-Handbuch.pdf"
-curl https://heyalter.com/wp-content/uploads/2021/02/HEY-HILFE-Support-Handbuch.pdf --output "$ISO_EXTRACTED_DIR/setup/HEY-HILFE-Support-Handbuch.pdf" --fail --location --max-time 60
+curl https://heyalter.com/wp-content/uploads/2021/02/HEY-HILFE-Support-Handbuch.pdf --output "$ISO_EXTRACTED_DIR/setup/HEY-HILFE-Support-Handbuch.pdf" --fail --location --max-time 60 & 
+
+log "Download Snaps"
+
+for snap2install in core18 core24 chromium teams-for-linux skype discord telegram-desktop
+do
+    echo download $snap2install
+    snap download $snap2install --target-directory "$ISO_EXTRACTED_DIR/setup/snaps"/
+    mv "$ISO_EXTRACTED_DIR/setup/snaps"/${snap2install}_*.snap "$ISO_EXTRACTED_DIR/setup/snaps"/${snap2install}.snap
+    mv "$ISO_EXTRACTED_DIR/setup/snaps"/${snap2install}_*.assert "$ISO_EXTRACTED_DIR/setup/snaps"/${snap2install}.assert
+done
+
